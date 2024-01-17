1d2983931fb36baf407e4bad7f799de0663f66f8
KiraWashi <leopaugam29@gmail.com>
2024-01-17 08:29:00 +0100

    personne-feature

--- src/yo.js
+++ src/yo.js
@@ -1,5 +1,19 @@
-const a = 12;
-let b = 45;
+let aPerson = {
+    name: 'Jean',
+    age: 35,
+    gender: 'masculin',
+    interest: ['musique', 'Jeux videos'],
+};
 
-b += a ;
-console.log("resultat de a + b = " + b);
+// on peut également déclarer et initialiser un objet de la façon suivante :
+
+let aPerson= new Object();
+aPerson.name = 'Jean';
+aPerson.age = 35;
+aPerson.gender = 'masculin';
+aPerson.interest = ['musique', 'badminton'];
+
+
+aPerson.sayHello = function() {console.log('Bonjour ! Je suis ' + this.name + '.'); };
+
+aPerson.sayHello();
